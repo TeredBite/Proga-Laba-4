f6af431bf2d4d82b27dec9861dd600d8ebfcbae0
Thinkpad <Thinkpad@THINKPADFOREVER>
2025-04-21 11:09:05 +0300

    .

--- Proga Laba 4/Proga Laba 4.cpp
+++ Proga Laba 4/Proga Laba 4.cpp
@@ -1,6 +1,37 @@
 ﻿#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+
+struct Invoice
+{
+	char Seller[6];
+	char Buyer[6];
+	char Amount[100];
+	unsigned int Year;
+	unsigned int Month;
+	unsigned int Day;
+};
+
+Invoice ReadFromFile(char* filename, int n)
+{
+	vector <Invoice> Records;
+	ifstream file(filename);
+	if (!file.is_open()) {
+		cerr << "Не удалось открыть файл. " << filename << endl;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		file.getline(Records[i].Seller, 6);
+		file.getline(Records[i].Buyer, 6);
+		file.getline(Records[i].Amount, 100);
+	}
+
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	
 }
